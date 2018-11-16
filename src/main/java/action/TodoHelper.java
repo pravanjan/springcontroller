7595a772b3bf9e73fde697f9bd057a5a8e1ca788
pravanjan <pravanjan.niranjan@a-cti.com>
2018-11-16 12:15:05 +0530

    adding the src foler changes

---
+++ src/main/java/action/TodoHelper.java
@@ -0,0 +1,1 @@
+package action;import java.util.HashMap;import java.util.Map;public class TodoHelper {    public Map createTask (String type){        Map<String,Object> paramMap = new HashMap<>();        paramMap.put("status",true);        paramMap.put("type",type);        return paramMap;    }}
